8a542557e1122fd03eb1de6661a96a2e07453231
josephius x <josephiusx@gmail.com>
2022-10-18 22:25:45 -0700

    63 Handling Different Routes

--- NodeJs_Max/Sec5-Express/app.js
+++ NodeJs_Max/Sec5-Express/app.js
@@ -4,18 +4,18 @@ const express = require('express');
 
 const app = express();
 
-// Middleware is what express is all about
-// Allows us to run functions via requst , and send a response
-// Allows us to add in Third party functionality
-// next allows us to continue one to the next middleware after the req,res
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In the middleware!');
-    next(); // To the next middleware
+    next();
 });
 
-
-app.use((req, res, next) => {
+app.use('/addProduct', (req, res, next) => {
     console.log('Another middleware!');
+    res.send(`<h1> add product page </h1>`) // sending
+});
+
+app.use('/', (req, res, next) => {
+    console.log('Another Other middleware!');
     res.send(`<h1> Hello from Express!</h1>`) // sending
 });
 
