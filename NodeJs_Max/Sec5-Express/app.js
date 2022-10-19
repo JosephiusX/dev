9f01b15d8c8dd71ec6d5773c77b339fb6ed91505
josephius x <josephiusx@gmail.com>
2022-10-18 21:38:23 -0700

    57 - 62 Express intro

--- NodeJs_Max/Sec5-Express/app.js
+++ NodeJs_Max/Sec5-Express/app.js
@@ -7,7 +7,17 @@ const app = express();
 // Middleware is what express is all about
 // Allows us to run functions via requst , and send a response
 // Allows us to add in Third party functionality
-app.use((req, res, next) => {});
+// next allows us to continue one to the next middleware after the req,res
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // To the next middleware
+});
+
+
+app.use((req, res, next) => {
+    console.log('Another middleware!');
+    res.send(`<h1> Hello from Express!</h1>`) // sending
+});
 
 const server = http.createServer(app);
 
