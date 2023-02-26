3251ccaee3746feebb8143831d86f781ed1169d1
josephius x <josephiusx@gmail.com>
2023-02-25 17:12:20 -0800

    updated Emaily

--- ReactExprMongo/server/client/src/components/Header.js
+++ ReactExprMongo/server/client/src/components/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
         <div className="nav-wrapper">
           <a href="#" className="brand-logo">Emaily</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="sass.html">Login With Google</a></li>
+            <li><a href="#">Login With Google</a></li>
           </ul>
         </div>
       </nav>
